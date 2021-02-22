46515c7461cf56d31f7a5f6153566e37bcef2b12
Shubhamb7 <shubhamb756@gmail.com>
2021-02-22 16:11:20 +0530

    minor changes

--- count.py
+++ count.py
@@ -5,22 +5,21 @@ import argparse
 new_parser = argparse.ArgumentParser()
 
 new_parser.add_argument("-s", "--start")
-new_parser.add_argument("-p", "--path")
 
 args = new_parser.parse_args()
 
-fileName = args.path + args.start + "-character-iteration-part-*.txt"
+fileName = "data/" + args.start + "-characters/" + str(args.start) + "-character-iteration-part-*.txt"
 fileNames = glob.glob(fileName)
 
 if len(fileNames) == 0:
-    print("no files for " + str(args.start) + " characters")
+    print("no files for " + str(args.start) + " characters in directory: " + str(os.getcwd()) + "data/" + str(args.start) + "-characters/")
     exit()
 
 total = 0
 
 for j in range(0,len(fileNames)):
 
-    file = open(args.path + args.start + "-character-iteration-part-" + str(j+1) + ".txt", "r") 
+    file = open("data/" + args.start + "-characters/" + str(args.start) + "-character-iteration-part-" + str(j+1) + ".txt", "r") 
 
     Content = file.read() 
     CoList = Content.split("\n") 
--- wordlist.py
+++ wordlist.py
@@ -1,16 +1,16 @@
 from itertools import product
 from string import ascii_lowercase
-import argparse
 import multiprocessing as mp
-import threading
 import datetime
+import argparse
+import os
     
 
 def writer(wordsList, filename):
 
     startTime = datetime.datetime.now()
 
-    f = open(filename, "a")
+    f = open(filename, "a+")
 
     for i in wordsList:
         f.write(i + "\n")
@@ -29,12 +29,13 @@ def txtWriter(start, batch, path):
 
     for i in product(ascii_lowercase, repeat = start):
 
+        print(i)
         wordsList.append("".join(i))
         count += 1
 
         if count % batch == 0 and count > 1: 
 
-            filename = path + str(start) + "-character-iteration-part-" + str(count//batch) + ".txt"
+            filename = path + str(start) + "-characters/" + str(start) + "-character-iteration-part-" + str(count//batch) + ".txt"
 
             p = mp.Process(target = writer, args=(wordsList, filename))
             p.start()
@@ -43,7 +44,7 @@ def txtWriter(start, batch, path):
             wordsList.clear()
 
 
-    filename = path + str(start) + "-character-iteration-part-" + str((count//batch)+1) + ".txt"
+    filename = path + str(start) + "-characters/" + str(start) + "character-iteration-part-" + str((count//batch)+1) + ".txt"
     p = mp.Process(target = writer, args=[wordsList, filename])
     processes.append(p)
     p.start()
@@ -57,6 +58,7 @@ if __name__ == "__main__":
     new_parser = argparse.ArgumentParser(description = "Create word list of combination a-z with number of character input.")
 
     new_parser.add_argument("-s", "--start", help = "character start count", type = int)
+    new_parser.add_argument("-e", "--end", help = "character end count", type = int)
     new_parser.add_argument("-p", "--path", help = "path in which the files will be stored", type = str)
     new_parser.add_argument("-b", "--batch", help = "enter the batches in which the data is to be split", type = int)
 
@@ -64,7 +66,25 @@ if __name__ == "__main__":
 
     startTime = datetime.datetime.now()
 
-    txtWriter(arguments.start, arguments.batch, arguments.path)
+    start = arguments.start
+    end = arguments.end
+
+    for i in range(start, end+1):
+        if os.path.exists("data/" + str(i) + "-characters/"):
+            pass
+        else:
+            os.mkdir("data/" + str(i) + "-characters/")
+
+    iterProcesses = []
+
+    for i in range(0, end+1 - start):
+        p = mp.Process(target = txtWriter, args = [start, arguments.batch, arguments.path])
+        p.start()
+        iterProcesses.append(p)
+        start += 1
+
+    for i in iterProcesses:
+        i.join()
 
     endTime = datetime.datetime.now()
 
